b667360d27afea0062142c92b5cc973983656c24
jhonny16 <tinny.1609@gmail.com>
2021-12-23 12:07:29 -0500

    customized v:14.0.0.2

--- l10n_cr_gonzales/__manifest__.py
+++ l10n_cr_gonzales/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '14.0.0.1',
+    'version': '14.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base','account','sale',],
@@ -24,7 +24,9 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/groups.xml',
         'views/account_move_views.xml',
+        'views/sale_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ l10n_cr_gonzales/models/sale_order.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+
+class SaleOrder(models.Model):
+    _name = "sale.order"
+    _inherit = "sale.order"
+
+    state = fields.Selection([
+        ('created','Borrador'),
+        ('draft', 'Quotation'),
+        ('sent', 'Quotation Sent'),
+        ('sale', 'Sales Order'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled'),
+    ], string='Estado', readonly=True, copy=False, index=True, tracking=3, default='created')
+
+    is_revised = fields.Boolean('Revisado') #state "CREATED"
+
+    #Todo: SE AÑADE EL CAMPO CREATED A ATRIBUTO STATES.
+    date_order = fields.Datetime(string='Order Date', required=True, readonly=True, index=True,
+                                 states={'draft': [('readonly', False)], 'sent': [('readonly', False)],
+                                         'created': [('readonly', False)]},
+                                 copy=False, default=fields.Datetime.now,
+                                 help="Creation date of draft/sent orders,\nConfirmation date of confirmed orders.")
+
+    def _default_validity_date(self):
+        if self.env['ir.config_parameter'].sudo().get_param('sale.use_quotation_validity_days'):
+            days = self.env.company.quotation_validity_days
+            if days > 0:
+                return fields.Date.to_string(datetime.now() + timedelta(days))
+        return False
+
+    validity_date = fields.Date(string='Expiration', readonly=True, copy=False, states={'draft': [('readonly', False)], 'sent': [('readonly', False)],
+                                                                                        'created': [('readonly', False)]},default=_default_validity_date)
+    partner_id = fields.Many2one(
+        'res.partner', string='Customer', readonly=True,
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)],
+                'created': [('readonly', False)]},
+        required=True, change_default=True, index=True, tracking=1,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", )
+    partner_invoice_id = fields.Many2one(
+        'res.partner', string='Invoice Address',
+        readonly=True, required=True,
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'sale': [('readonly', False)],
+                'created': [('readonly', False)]},
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", )
+    partner_shipping_id = fields.Many2one(
+        'res.partner', string='Delivery Address', readonly=True, required=True,
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'sale': [('readonly', False)],
+                'created': [('readonly', False)]},
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", )
+    pricelist_id = fields.Many2one(
+        'product.pricelist', string='Pricelist', check_company=True,  # Unrequired company
+        required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)],
+                                              'created': [('readonly', False)]},
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", tracking=1,
+        help="If you change the pricelist, only newly added lines will be affected.")
+    analytic_account_id = fields.Many2one(
+        'account.analytic.account', 'Analytic Account',
+        readonly=True, copy=False, check_company=True,  # Unrequired company
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)],
+                'created': [('readonly', False)]},
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        help="The analytic account related to a sales order.")
+
+
+    #Pasar estado a Revisado
+    def action_revised(self):
+        if self.state == 'created':
+            self.state = 'draft'
+            self.is_revised = True
